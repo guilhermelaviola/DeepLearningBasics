313de592abfa80c6c021c1bb6ae12b7dd5e40ad0
guilherme <guilhermelaviola96@gmail.com>
2023-02-13 20:40:30 -0300

    Added training to the model with two neurons

--- training_neurons/multiple_neurons_network.py
+++ training_neurons/multiple_neurons_network.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.optimizers import SGD
 
 # Creating a dataset
 bad_pizza_dataset = pd.DataFrame.from_dict({
@@ -22,3 +26,37 @@ bad_pizza_dataset.plot(
     c="c_result",
     colormap="jet"
 )
+
+plt.show()
+
+#Configuring and training a simple neural network
+# Configuring the layers
+input_layer = Dense(units=2, input_dim=2, activation="sigmoid")
+output_layer = Dense(units=1, activation="sigmoid")
+
+# Configuring the model
+bad_pizza_model = Sequential()
+bad_pizza_model.add(input_layer)
+bad_pizza_model.add(output_layer)
+sgd = SGD()
+bad_pizza_model.compile(loss="binary_crossentropy", optimizer=sgd, metrics=["accuracy"])
+
+# Displaying the configuration
+# It shows 9 trainable parameters
+bad_pizza_model.summary()
+
+# Training the model
+# The 'epoch' parameter indicates how many times the network
+# must calculate the training data
+history = bad_pizza_model.fit(
+    bad_pizza_dataset[["c_tomato_sauce", "c_barbecue_sauce"]],
+    bad_pizza_dataset["c_result"],
+    epochs=3000,
+)
+
+# Finding out the model performance
+test_loss, test_accuracy = bad_pizza_model.evaluate(
+    bad_pizza_dataset[["c_tomato_sauce", "c_barbecue_sauce"]],
+    bad_pizza_dataset["c_result"]
+)
+print(f"Evaluation result on Test Data : Loss = {test_loss}, accuracy = {test_accuracy}")
